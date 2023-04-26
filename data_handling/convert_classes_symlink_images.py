2c9b438aa5c82a0fb60ca54f6f8403848c21628a
Dorian Tsai <dorian.tsai@gmail.com>
2023-04-26 10:33:43 +1000

    added symlink functionality to preserve original dataset

---
+++ data_handling/convert_classes_symlink_images.py
@@ -0,0 +1,65 @@
+#! /usr/bin/env/python3
+
+"""convert_classes.py
+script to convert yolov5 classes to desired class
+also create symbolic links of images into the same folder (could copy/paste, but issues with memory)
+we want this to have all labels turtles for initial turtle detector, but want to
+retain the painted/not-painted labels for groundtruth and 
+"""
+
+
+import os
+import glob
+
+# set folder for labels
+label_dir = '/home/dorian/Code/turtles/turtle_datasets/job10_041219-0-1000/obj_train_data'
+
+# TODO should manage classes via dictionary
+# set class label that needs changing
+label_to_change = '1'
+
+# set desired class label to change to (output class)
+label_default = '0'
+
+# set output directory
+output_dir = '/home/dorian/Code/turtles/turtle_datasets/job10_041219-0-1000/turtles_labels_only'
+os.makedirs(output_dir, exist_ok=True)
+
+
+# create symbolic links of images into output directory
+# NOTE separate from the label_files, for modularity
+print('creating symlinks of images')
+img_files = glob.glob(label_dir + '/*.PNG')
+for i, img_file in enumerate(img_files):
+    print(f'{i+1}/{len(img_files)}: {os.path.basename(img_file)}')
+    link_path = os.path.join(output_dir, os.path.basename(img_file))
+    os.symlink(img_file, link_path)
+
+
+# iterate through all files in given folder
+# label_files = sorted(os.listdir(label_dir))
+print('converting classes in label text files')
+label_files = glob.glob(label_dir + '/*.txt')
+for i, label_file in enumerate(label_files):
+    print(f'{i+1}/{len(label_files)}: {os.path.basename(label_file)}')
+    
+    # output file name
+    output_file = os.path.basename(label_file)
+    # input file
+    with open(os.path.join(label_file), 'r') as infile, \
+        open(os.path.join(output_dir, output_file), 'w') as outfile:
+            # loop through each line of the input file
+            for line in infile:
+                # replace character if it is not the desired class:
+                if line[0] == label_to_change:
+                    modified_line = line.replace(label_to_change, label_default, 1)
+                else:
+                    modified_line = line
+                
+                # write to output file
+                outfile.write(modified_line)
+
+print('done')
+
+import code
+code.interact(local=dict(globals(), **locals()))
--- data_handling/cvat2trainv2.py
+++ data_handling/cvat2trainv2.py
@@ -8,7 +8,7 @@ import shutil
 import random
 import os
 
-imgno = 1000
+imgno = 1000 # NOTE not all jobs have 1000 images
 train_ratio = 0.8
 test_ratio = 0.1
 valid_ratio = 0.1
@@ -20,13 +20,13 @@ def check_ratio(test_ratio,train_ratio,valid_ratio):
     if not((train_ratio+test_ratio+valid_ratio)==1): ValueError("sum of train/val/test ratio must equal 1")
 check_ratio(test_ratio,train_ratio,valid_ratio)
 
-retrievepath = os.path.join('/home/raineai/Downloads','obj_train_data')
-savepath = os.path.join('/home/raineai/Turtles/datasets','job11')
+retrievepath = os.path.join('/home/dorian/Code/turtles/turtle_datasets','job10_041219-0-1000', 'turtles_labels_only')
+savepath = os.path.join('/home/dorian/Code/turtles/turtle_datasets','job10_041219-0-1000','split_data')
 
 def clean_dirctory(savepath):
     if os.path.isdir(savepath):
         shutil.rmtree(savepath)
-    os.makedirs(savepath)
+    os.makedirs(savepath, exist_ok=True)
 clean_dirctory(savepath)
 
 imagelist = glob.glob(os.path.join(retrievepath, '*.PNG'))
@@ -51,7 +51,7 @@ for i, text in enumerate(txtlist):#convert all labels to turtles (0)
             f.write(f'{a} {b} {c} {d} {e}\n')
 
 def seperate_files(number,imglist,textlist):
-    for i in range(number):
+    for i in range(int(number)):
         r = random.randint(0, len(textlist))
         imglist.append(imagelist[r])
         textlist.append(txtlist[r])
@@ -62,14 +62,27 @@ def seperate_files(number,imglist,textlist):
 seperate_files(imgno*valid_ratio,validimg,validtext) #get some valid images
 seperate_files(imgno*test_ratio,testimg,testtext)
 
+# function to preserve symlinks of src file, otherwise default to copy
+def copy_link(src, dst):
+    if os.path.islink(src):
+        linkto = os.readlink(src)
+        os.symlink(linkto, os.path.join(dst, os.path.basename(src)))
+    else:
+        shutil.copy(src, dst)
+        
+
 def move_file(filelist,savepath,second_path):
+    output_path = os.path.join(savepath, second_path)
+    clean_dirctory(output_path)
+    os.makedirs(output_path, exist_ok=True)
     for i, item in enumerate(filelist):
-        shutil.move(item, os.path.join(savepath,second_path))
+        # shutil.move(item, os.path.join(savepath,second_path))
+        copy_link(item, output_path)
 
 move_file(txtlist,savepath,'train/labels')
 move_file(imagelist,savepath,'train/images')
 move_file(validtext,savepath,'valid/labels')
 move_file(validimg,savepath,'valid/images')
-move_file(testimg,savepath,'test/labels')
-move_file(testtext,savepath,'test/images')
+move_file(testimg,savepath,'test/images')
+move_file(testtext,savepath,'test/labels')
     
