2ad75c466f46981c6302d12db104c417d3f03593
Alec Tutin <a.tutin@qut.edu.au>
2023-08-29 17:15:50 +1000

    Clearing track information on setup.

--- tracker/SMTrackingPipeline.py
+++ tracker/SMTrackingPipeline.py
@@ -74,6 +74,7 @@ class Pipeline():
 
     def setup(self, video_in_path: str, output_dir_path: str, detection_model_name: Optional[str] = None, classification_model_name: Optional[str] = None) -> None:
         self.frame_index = 0
+        self.tracks.clear()
         self.video_path = os.path.expanduser(video_in_path)
         self.output_dir_path = os.path.expanduser(output_dir_path)
 
